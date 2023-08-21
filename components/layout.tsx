0f2fafc357cb9d60d68ba88aaa0e78b3f0244818
Joon Nam <jungeol66014@gmail.com>
2023-08-21 15:14:36 +0900

    added and deleted js with ts

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,22 +5,29 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Joon Nam";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Blog | Joon Nam";
 
-export default function Layout({ children, home }) {
+export default function Layout({
+	children,
+	home,
+}: {
+	children: React.ReactNode;
+	home?: boolean;
+}) {
 	return (
 		<div className={styles.container}>
 			<Head>
 				<link rel="icon" href="/favicon.ico" />
 				<meta
 					name="description"
-					content="Learn how to build a personal website using Next.js"
+					content="Interested in overcoming obstacles, that is surviving."
 				/>
 				<meta
 					property="og:image"
-					content={`https://og-image.vercel.app/${encodeURI(
-						siteTitle
-					)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+					content="/images/profile.jpg"
+					// content={`https://og-image.vercel.app/${encodeURI(
+					// 	siteTitle
+					// )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
 				/>
 				<meta name="og:title" content={siteTitle} />
 				<meta name="twitter:card" content="summary_large_image" />
