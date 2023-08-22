52a13e862e8a0ec8353e14b536dc818531b7e787
Joon Nam <jungeol66014@gmail.com>
2023-08-22 09:51:17 +0900

    made fetched md files to break new line automatically

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	console.log(res);
 	res.status(200).json({ text: "Hello" });
 }
